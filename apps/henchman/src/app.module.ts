fd60dfa1cba37c720316a94fadd1fb6dd3fa05e5
sameer <sameer.khandke@gmail.com>
2026-05-12 12:03:25 +0000

    minor changes

--- apps/henchman/src/app.module.ts
+++ apps/henchman/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { BullModule } from '@nestjs/bullmq';
 import { mailConfig, queueConfig, loggerConfig } from './config';
 import { LoggerModule } from 'nestjs-pino';
 import { MailService } from './mail/mail.service';
@@ -17,6 +18,16 @@ import { MailQueueConsumer } from './consumers/mail-queue.consumer';
             useFactory( configService: ConfigService ) {
                 return configService.getOrThrow( 'logger.config' );
             },
+        } ),
+        BullModule.forRootAsync( {
+            imports: [ ConfigModule ],
+            inject: [ ConfigService ],
+            useFactory: ( configService: ConfigService ) => ( {
+                connection: configService.get( 'queue.redis' ),
+            } ),
+        } ),
+        BullModule.registerQueue( {
+            name: 'mail',
         } ),
     ],
     providers: [ MailService, MailQueueConsumer ],
